297f76c9cf394cfa1f4b13f608a7fec01c24649b
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2024-03-08 21:26:27 -0300

    Versão 01

---
+++ src/main/java/n1/Avalicao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package n1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Avalicao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
